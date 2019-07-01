ce4cd042eac27442b74cbb2102c66d123007a165
u1 <u1@c1.fritz.box>
2019-07-01 17:03:05 +0200

    moved errorprone in

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module com.github.gv2011.guava {
   requires org.checkerframework.checker.qual;
-  requires error.prone.annotations;
+//  requires error.prone.annotations;
   requires java.logging;
   requires jsr305;
   requires j2objc.annotations;
+  requires java.compiler;
 //  requires jdk.unsupported;
 //  requires failureaccess;
 
